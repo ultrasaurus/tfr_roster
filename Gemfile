8e7fd9a09de305d97214875fb1f8b6cd718c73fd
Sarah Allen <sarah@ultrasaurus.com>
2011-01-02 08:31:09 -0800

    changes in roster app for ActiveRecord chapter

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
 # and rake tasks are available in development mode:
-# group :development, :test do
-#   gem 'webrat'
-# end
+# START:test
+group :development, :test do
+  gem "rspec-rails", "2.3.1"
+end
+# END:test
+
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,20 @@
+# START:method
+# START:validates
 # START:class
 class Person < ActiveRecord::Base
 # END:class
+# END:method
+  validates_presence_of :first_name
+# END:validates
+  validates_presence_of :last_name
 
+# START:method
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+# START:validates
 # START:class
 end
 # END:class
+# END:validates
+# END:method
